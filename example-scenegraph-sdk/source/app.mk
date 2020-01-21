add406edd48ba7aa67ebf366fcf0074dc662cc2e
samdozor <sdozor@mparticle.com>
2020-01-21 18:26:58 -0500

    Fix makefile ping for Mac

--- example-scenegraph-sdk/source/app.mk
+++ example-scenegraph-sdk/source/app.mk
@@ -135,7 +135,7 @@ ifeq ($(HOST_OS),cygwin)
 	# This assumes that the Windows ping command is used, not cygwin's.
 	QUICK_PING_ARGS = -n 1 -w 1000
 else # Linux
-	QUICK_PING_ARGS = -c 1 -w 1
+	QUICK_PING_ARGS = -c 1
 endif
 
 ifndef ZIP_EXCLUDE
